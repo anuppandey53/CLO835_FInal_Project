e95c5a8d210be46991568edb70a6b01f461097b3
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-12-07 00:55:57 -0500

    Create config.tf
    created terraform config.tf code

---
+++ terraform/dev/instances/config.tf
@@ -0,0 +1,28 @@
+# # # # # # # # # # # # # # # # #
+# CLO835 final_project          #
+# Group 05                      #
+# # # # # # # # # # # # # # # # #
+
+#Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Data block to retrieve the default VPC id 
+data "aws_vpc" "default" {
+  default = true
+
+  # tags = {
+  #   Name = "default vpc"
+  # }
+}
